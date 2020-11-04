b9c1c001a6c8e2bb7085928408d9ab6378aad680
Peter Slobodin <bkDezmond@gmail.com>
2020-11-04 11:24:43 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
 "use strict";
-const MIN_LIKES = 15;
-const MAX_LIKES = 200;
-const MIN_COMMENTS = 1;
-const MAX_COMMENTS = 5;
+
+let getRandomNumber = (minNumber, maxNumber) => {
+  return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+};
 
 let NAMES = [`Игорь`, `Андрей`, `Илья`, `Анна`, `Ирина`, `Ольга`];
 let getRandomName = () => {
-  let randomIndex = Math.floor(Math.random() * NAMES.length);
+  let randomIndex = getRandomNumber(0, NAMES.length);
   return NAMES[randomIndex];
 };
 
 let MESSAGES = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`, `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`, `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`, `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`];
 let getRandomMessage = () => {
-  let randomIndex = Math.floor(Math.random() * MESSAGES.length);
+  let randomIndex = getRandomNumber(0, MESSAGES.length);
   return MESSAGES[randomIndex];
 };
 
 let AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6];
 
 let getRandomAvatar = () => {
-  let randomIndex = Math.floor(Math.random() * AVATAR_NUMBERS.length);
+  let randomIndex = getRandomNumber(0, AVATAR_NUMBERS.length);
   let avatarSrc = `img/avatar-` + AVATAR_NUMBERS[randomIndex] + `.svg`;
   return avatarSrc;
 };
-
 
 let getSomeComment = () => {
   return {
@@ -33,13 +32,9 @@ let getSomeComment = () => {
   };
 };
 
-let getRandomNumber = () => {
-  return Math.floor(Math.random() * (MAX_COMMENTS - MIN_COMMENTS)) + MIN_COMMENTS;
-};
-
 let getRandomComments = () => {
   const randomComments = [];
-  const randomCount = getRandomNumber();
+  const randomCount = getRandomNumber(1, 5);
   for (let i = 0; i < randomCount; i++) {
     const comment = getSomeComment();
     randomComments.push(comment);
@@ -47,15 +42,11 @@ let getRandomComments = () => {
   return randomComments;
 };
 
-let getRandomLikes = () => {
-  return Math.floor(Math.random() * (MAX_LIKES - MIN_LIKES)) + MIN_LIKES;
-};
-
 let getSomePhoto = (url) => {
   return {
     url,
     description: `description`,
-    likes: getRandomLikes(),
+    likes: getRandomNumber(15, 200),
     comments: getRandomComments(),
   };
 };
@@ -70,19 +61,19 @@ let generateRandomPhotos = () => {
 
 let photos = generateRandomPhotos();
 
-let pictureElements = document.querySelector(`.pictures`);
+let picturesElements = document.querySelector(`.pictures`);
 let similarPictureTemplate = document.querySelector(`#picture`).content;
 
 // функция создания DOM
 
-let renderTemplateContent = (photo) => {
+let generatePhotoElement = (photo) => {
   let pictureTemplate = similarPictureTemplate.cloneNode(true);
-  let pictureComments = pictureTemplate.querySelector(`.picture__comments`);
-  let pictureLikes = pictureTemplate.querySelector(`.picture__likes`);
-  let pictureImages = pictureTemplate.querySelector(`.picture__img`);
-  pictureComments.textContent = photo.comments.length;
-  pictureLikes.textContent = photo.likes;
-  pictureImages.src = photo.url;
+  let pictureCommentsCount = pictureTemplate.querySelector(`.picture__comments`);
+  let pictureLikesCount = pictureTemplate.querySelector(`.picture__likes`);
+  let pictureImagesCount = pictureTemplate.querySelector(`.picture__img`);
+  pictureCommentsCount.textContent = photo.comments.length;
+  pictureLikesCount.textContent = photo.likes;
+  pictureImagesCount.src = photo.url;
   return pictureTemplate;
 };
 
@@ -90,7 +81,6 @@ let renderTemplateContent = (photo) => {
 
 let fragment = document.createDocumentFragment();
 for (let i = 0; i < photos.length; i++) {
-  fragment.appendChild(renderTemplateContent(photos[i]));
+  fragment.appendChild(generatePhotoElement(photos[i]));
 }
-pictureElements.appendChild(fragment);
-
+picturesElements.appendChild(fragment);
